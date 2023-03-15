977605ede07052ef19eee51b594378e32a7dccb8
Milind-Srivastava2002 <125573324+Milind-Srivastava2002@users.noreply.github.com>
2023-03-15 22:40:25 +0530

    Add files via upload

---
+++ Coming Soon/Coming_Soon-main/last/src/index.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { ComingSoon } from "./screens/ComingSoon";
+
+const app = document.getElementById("app");
+ReactDOM.render(<ComingSoon />, app);
---
+++ Coming Soon/Coming_Soon-main/last/src/screens/ComingSoon/ComingSoon.jsx
@@ -0,0 +1,192 @@
+import React from "react";
+import "./style.css";
+
+export const ComingSoon = () => {
+  return (
+    <div className={"COMING-SOON-COMING-SOON-wrapper"}>
+      <div className={"COMING-SOON-COMING-SOON"}>
+        <div className={"COMING-SOON-overlap"}>
+          <div className={"COMING-SOON-overlap-group2"}>
+            <div className={"COMING-SOON-rectangle"} />
+            <div className={"COMING-SOON-rectangle-45"} />
+            <div className={"COMING-SOON-rectangle-49"} />
+            <div className={"COMING-SOON-rectangle-50"} />
+            <div className={"COMING-SOON-rectangle-48"} />
+            <p className={"COMING-SOON-first-name-last-name"}>
+              First Name &amp; Last Name
+            </p>
+            <div className={"COMING-SOON-phone-number"}>Phone Number</div>
+            <div className={"COMING-SOON-email-address"}>Email Address</div>
+            <div className={"COMING-SOON-message-or-question"}>
+              Message or Question
+            </div>
+            <div className={"COMING-SOON-rectangle-51"} />
+            <div className={"COMING-SOON-SUBMIT"}>SUBMIT</div>
+            <div className={"COMING-SOON-rectangle-68"} />
+          </div>
+          <p className={"COMING-SOON-got-questions-we-will-love-to-hear-them"}>
+            <span className={"COMING-SOON-text-wrapper"}>
+              Got questions?
+              <br />
+            </span>
+            <span className={"COMING-SOON-span"}>
+              We will love to hear them!
+            </span>
+          </p>
+          <img
+            className={"COMING-SOON-iconsax-linear-messagequestion"}
+            src={"/img/iconsax-linear-messagequestion-1.svg"}
+          />
+        </div>
+        <div className={"COMING-SOON-overlap-group4"}>
+          <div className={"COMING-SOON-rectangle-42"} />
+          <div className={"COMING-SOON-CONTACT-US"}>CONTACT US</div>
+          <div className={"COMING-SOON-rectangle-43"} />
+          <div className={"COMING-SOON-rectangle-44"} />
+          <img
+            className={"COMING-SOON-LOGOEMBLEM"}
+            src={"/img/logoemblem-2-1.png"}
+          />
+          <h1 className={"COMING-SOON-h-1"}>COMING SOON!</h1>
+        </div>
+        <div className={"COMING-SOON-overlap-group3"}>
+          <p
+            className={
+              "COMING-SOON-element-olympic-indoor-sports-powered-by-eazy-eyez-entertainment"
+            }
+          >
+            <br />© 2023 Olympic Indoor Sports.&nbsp;&nbsp;Powered by Eazy Eyez
+            Entertainment.
+          </p>
+        </div>
+        <div className={"COMING-SOON-overlap-group5"}>
+          <p
+            className={
+              "COMING-SOON-olympic-indoor-sports-a-game-changing-platform-for-young-athletes-in-maryland-to-step-up-and-show-off-their-skills-in-sports-such-as-baseball-softball-cricket-and-lacrosse-our-mission-is-to-provide-a-comprehensive-space-where-these-young-rising-stars-can-take-a-swing-at-their-athletic-aspirations-and-score-big-in-the-game-of-life-we-re-proud-to-be-the-home-plate-for-the-future-generation-of-american-sports-and-we-hope-to-establish-a-precedent-for-other-players-in-the-industry-so-come-on-over-join-the-team-and-let-s-play-ball"
+            }
+          >
+            Olympic Indoor Sports, a game-changing platform for young athletes
+            in Maryland <br />
+            to step up and show off their skills in sports such as baseball,
+            softball, cricket, and lacrosse. <br />
+            <br />
+            Our mission is to provide a comprehensive space where these young
+            rising stars can take a swing at their athletic aspirations and
+            score big in the game of life. <br />
+            <br />
+            We&#39;re proud to be the home plate for the future generation of
+            American sports, and we hope to establish a precedent for other
+            players in the industry. So come on over, join the team, and
+            let&#39;s play ball!
+          </p>
+          <div className={"COMING-SOON-the-facility"}>The Facility</div>
+          <p
+            className={
+              "COMING-SOON-olympic-indoor-sports-red-branch-rd-suite-a-columbia-MD"
+            }
+          >
+            <span className={"COMING-SOON-text-wrapper-2"}>
+              Olympic Indoor Sports
+              <br />
+            </span>
+            <span className={"COMING-SOON-text-wrapper-3"}>
+              <br />
+              9179 Red Branch rd Suite #A Columbia, MD 21045
+            </span>
+          </p>
+          <p
+            className={
+              "COMING-SOON-element-AM-EST-PM-EST-from-monday-to-sunday-you-will-receive-a-prompt-response-from-our-team-within-hours"
+            }
+          >
+            <span className={"COMING-SOON-text-wrapper-4"}>
+              7:00 AM EST - 11:00 PM EST
+            </span>
+            <span className={"COMING-SOON-text-wrapper-3"}>
+              {" "}
+              from Monday to Sunday. <br />
+              <br />
+              You will receive a prompt response from our team within 24 hours.
+            </span>
+          </p>
+          <div className={"COMING-SOON-overlap-group6"}>
+            <div
+              className={"COMING-SOON-email-info-olympicindoorsports-com-phone"}
+            >
+              <span className={"COMING-SOON-text-wrapper-4"}>Email: </span>
+              <span className={"COMING-SOON-text-wrapper-3"}>
+                info@olympicindoorsports.com
+                <br />
+              </span>
+              <span className={"COMING-SOON-text-wrapper-4"}>Phone: </span>
+              <span className={"COMING-SOON-text-wrapper-3"}>
+                (123) 456-7890
+                <br />
+              </span>
+            </div>
+            <div className={"COMING-SOON-group"}>
+              <div className={"COMING-SOON-overlap-group"}>
+                <div className={"COMING-SOON-learn-more"}>Learn More</div>
+              </div>
+            </div>
+            <img
+              className={"COMING-SOON-LOGOEMBLEM-1"}
+              src={
+                "https://cdn.animaapp.com/projects/63ece14667db4bb19c023a64/releases/63edd8bbea18a32c49d2229e/img/logoemblem-2@2x.png"
+              }
+            />
+            <div className={"COMING-SOON-div"}>CONTACT US</div>
+          </div>
+          <div className={"COMING-SOON-memberships"}>Memberships</div>
+          <div className={"COMING-SOON-batting-cages"}>Batting Cages</div>
+          <div className={"COMING-SOON-programs-events"}>
+            Programs &amp; Events
+          </div>
+          <div className={"COMING-SOON-contact-us"}>Contact Us</div>
+          <div className={"COMING-SOON-QUICK-LINKS"}>QUICK LINKS</div>
+          <div className={"COMING-SOON-LOCATION"}>LOCATION</div>
+          <div className={"COMING-SOON-TIMINGS"}>TIMINGS</div>
+        </div>
+        <p className={"COMING-SOON-WE-ARE-BRINGIN-SOMETHING-AWESOME-YOUR-WAY"}>
+          WE ARE BRINGIN’ SOMETHING AWESOME YOUR WAY
+        </p>
+        <div className={"COMING-SOON-NAV-BAR"} style={{ width: "100vw" }}>
+          <div
+            className={"COMING-SOON-overlap-group1"}
+            style={{ width: "100%" }}
+          >
+            <div
+              className={"COMING-SOON-rectangle-33"}
+              style={{ width: "100%" }}
+            />
+            <div
+              className={"COMING-SOON-rectangle-5"}
+              style={{ width: "100%" }}
+            />
+            <a href="http://35.72.8.63:5678">
+              <div className={"COMING-SOON-home"}>Home</div>
+            </a>
+            <div className={"COMING-SOON-the-facility-2"}>The Facility</div>
+            <div className={"COMING-SOON-memberships-2"}>Memberships</div>
+            <div className={"COMING-SOON-batting-cages-2"}>Batting Cages</div>
+            <div className={"COMING-SOON-programs-events-2"}>
+              Programs &amp; Events
+            </div>
+            <div className={"COMING-SOON-contact-us-2"}>Contact Us</div>
+            <img
+              className={"COMING-SOON-iconsax-linear-usersquare"}
+              src={"/img/iconsax-linear-usersquare-2.svg"}
+            />
+            <div className={"COMING-SOON-SIGN-UP"}>SIGN UP</div>
+            <img
+              className={"COMING-SOON-OLYMPICLOGO"}
+              src={
+                "https://cdn.animaapp.com/projects/63ece14667db4bb19c023a64/releases/63ed44f8a1a58d92896d8c03/img/olympiclogo-1.png"
+              }
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
